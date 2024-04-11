04337ae3eeb8a10fb6ce19cab961c89f1f1c8bec
Azepp <zidanathallahwinata@gmail.com>
2024-04-11 19:51:42 +0700

    Test

--- react/src/Components/Sidebar/Sidebar.jsx
+++ react/src/Components/Sidebar/Sidebar.jsx
@@ -1,7 +1,10 @@
 function Sidebar() {
-    return ( 
-        <h1>Test</h1>
-     );
+    return (
+        <>
+            <h1>Test</h1>
+            <h1>Halo</h1>
+        </>
+    );
 }
 
 export default Sidebar;
