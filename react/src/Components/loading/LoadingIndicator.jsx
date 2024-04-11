b7f0dc3a6fcb1a02ca3abc07eb5a7ac1cc7b732e
cinosjuchi <pacino447@gmail.com>
2024-04-12 03:25:27 +0700

    loading

---
+++ react/src/Components/loading/LoadingIndicator.jsx
@@ -0,0 +1,28 @@
+import SyncLoader from "react-spinners/RiseLoader";
+import { TypeAnimation } from "react-type-animation";
+import React from "react";
+import { Typography } from "@material-tailwind/react";
+const LoadingIndicator = () => {
+    return (
+        <div className="bg-light-blue-100 h-screen flex flex-column justify-center items-center">
+            <Typography variant="h3" className="font-jakarta text-center">
+                <SyncLoader color="#fff" size={36} className="mb-16" />
+                <div>
+                    <TypeAnimation
+                        preRenderFirstString={true}
+                        sequence={[
+                            500,
+                            "Selamat Datang di Web Kwaldik", // initially rendered starting point
+                            1000,
+                            "Tunggu Beberapa Saat...",
+                        ]}
+                        speed={50}
+                        repeat={Infinity}
+                    />
+                </div>
+            </Typography>
+        </div>
+    );
+};
+
+export default LoadingIndicator;
--- react/src/Layouts/GuestLayout.jsx
+++ react/src/Layouts/GuestLayout.jsx
@@ -1,9 +1,17 @@
 import { Outlet } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import LoadingIndicator from "../Components/loading/LoadingIndicator";
 
 export default function GuestLayout() {
-    return (
-        <div>
-            <Outlet />
-        </div>
-    );
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Logika untuk memuat data atau sumber daya lainnya
+        // Setelah selesai memuat, atur loading menjadi false
+        setTimeout(() => {
+            setLoading(false);
+        }, 10000); // Contoh, ganti dengan logika memuat yang sesuai
+    }, []);
+
+    return <div>{loading ? <LoadingIndicator /> : <Outlet />}</div>;
 }
