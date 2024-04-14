5afe9eed11c8f22217915d13a502605b17b17fca
Azepp <zidanathallahwinata@gmail.com>
2024-04-14 21:24:41 +0700

    Chart

--- react/src/Components/Charts/ContainChart.jsx
+++ react/src/Components/Charts/ContainChart.jsx
@@ -2,7 +2,7 @@ import Example from "./LineDiagram";
 
 function ContainChart() {
     return (
-        <div className="flex gap-4">
+        <div className="grid grid-cols-3 gap-4">
             <Example></Example>
             <Example></Example>
             <Example></Example>
--- react/src/Components/Charts/LineDiagram.jsx
+++ react/src/Components/Charts/LineDiagram.jsx
@@ -5,17 +5,13 @@ import {
     Typography,
 } from "@material-tailwind/react";
 import Chart from "react-apexcharts";
-import { Square3Stack3DIcon } from "@heroicons/react/24/outline";
-
-// If you're using Next.js please use the dynamic import for react-apexcharts and remove the import from the top for the react-apexcharts
-// import dynamic from "next/dynamic";
-// const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+import { AcademicCapIcon } from "@heroicons/react/24/outline";
 
 const chartConfig = {
     type: "pie",
-    width: 280,
-    height: 280,
-    series: [44, 55, 13, 43, 22],
+    width: 320,
+    height: 320,
+    series: [40, 55, 13, 43, 22, 20],
     options: {
         chart: {
             toolbar: {
@@ -28,10 +24,18 @@ const chartConfig = {
         dataLabels: {
             enabled: false,
         },
-        colors: ["#020617", "#ff8f00", "#00897b", "#1e88e5", "#d81b60"],
+        colors: [
+            "#020617",
+            "#ff8f00",
+            "#00897b",
+            "#1e88e5",
+            "#d81b60",
+            "#fff000",
+        ],
         legend: {
-            show: false,
+            show: true,
         },
+        labels: ["Data 1", "Data 2", "Data 3", "Data 4", "Data 5", "Data 6"],
     },
 };
 
@@ -44,8 +48,8 @@ export default function Example() {
                 color="transparent"
                 className="flex flex-col gap-4 rounded-none md:flex-row md:items-center"
             >
-                <div className="w-max rounded-lg bg-gray-900 p-5 text-white">
-                    <Square3Stack3DIcon className="h-6 w-6" />
+                <div className="w-max rounded-lg bg-biru p-3 text-white font-bold">
+                    <AcademicCapIcon className="h-10 w-10" />
                 </div>
                 <div>
                     <Typography variant="h6" color="blue-gray">
@@ -57,15 +61,12 @@ export default function Example() {
                         className="max-w-sm font-normal"
                     >
                         Visualize your data in a simple way using the
-                        @material-tailwind/react chart plugin.
                     </Typography>
                 </div>
             </CardHeader>
             <CardBody className="mt-4 grid place-items-center px-2">
                 <Chart {...chartConfig} />
             </CardBody>
-      </Card>
-      
-      
+        </Card>
     );
 }
