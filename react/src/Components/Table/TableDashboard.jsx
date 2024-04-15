3d3e37a57fc53c7d6c99006f4eaee8da2e00e2c5
cinosjuchi <pacino447@gmail.com>
2024-04-15 22:21:11 +0700

    merge

--- react/src/Components/Table/TableDashboard.jsx
+++ react/src/Components/Table/TableDashboard.jsx
@@ -1,5 +1,8 @@
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { PencilIcon, UserPlusIcon } from "@heroicons/react/24/solid";
+import {
+    MagnifyingGlassIcon,
+    ChevronUpDownIcon,
+} from "@heroicons/react/24/outline";
+import { PencilIcon } from "@heroicons/react/24/solid";
 import {
     Card,
     CardHeader,
@@ -7,138 +10,130 @@ import {
     Typography,
     Button,
     CardBody,
-    Chip,
     CardFooter,
-    Tabs,
-    TabsHeader,
-    Tab,
-    Avatar,
     IconButton,
     Tooltip,
 } from "@material-tailwind/react";
 
-const TABS = [
-    {
-        label: "All",
-        value: "all",
-    },
-    {
-        label: "Monitored",
-        value: "monitored",
-    },
-    {
-        label: "Unmonitored",
-        value: "unmonitored",
-    },
+const TABLE_HEAD = [
+    "No",
+    "NRP",
+    "Nama",
+    "Pangkat",
+    "Dikum",
+    "Dikpol",
+    "Fungsi Polri",
+    "Diklat",
+    "Dikbangpes",
+    "Aksi",
 ];
-
-const TABLE_HEAD = ["Member", "Function", "Status", "Employed", ""];
 
 const TABLE_ROWS = [
     {
-        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg",
-        name: "John Michael",
-        email: "john@creative-tim.com",
-        job: "Manager",
-        org: "Organization",
-        online: true,
+        no: "1",
+        nrp: "123456789",
+        nama: "John Michael Lorem Ipsum",
+        pangkat: "Brada",
+        dikum: "S2",
+        dikpol: "Unknown",
+        fungsi: "Unknown",
+        diklat: "Unknown",
+        dikbangpes: "Unknown",
         date: "23/04/18",
     },
     {
-        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-2.jpg",
-        name: "Alexa Liras",
-        email: "alexa@creative-tim.com",
-        job: "Programator",
-        org: "Developer",
-        online: false,
+        no: "2",
+        nrp: "123456788",
+        nama: "John Michael",
+        pangkat: "Brada",
+        dikum: "S2",
+        dikpol: "Unknown",
+        fungsi: "Unknown",
+        diklat: "Unknown",
+        dikbangpes: "Unknown",
         date: "23/04/18",
     },
     {
-        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-1.jpg",
-        name: "Laurent Perrier",
-        email: "laurent@creative-tim.com",
-        job: "Executive",
-        org: "Projects",
-        online: false,
-        date: "19/09/17",
+        no: "3",
+        nrp: "123456788",
+        nama: "John Michael",
+        pangkat: "Brada",
+        dikum: "S2",
+        dikpol: "Unknown",
+        fungsi: "Unknown",
+        diklat: "Unknown",
+        dikbangpes: "Unknown",
+        date: "23/04/18",
     },
     {
-        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-4.jpg",
-        name: "Michael Levi",
-        email: "michael@creative-tim.com",
-        job: "Programator",
-        org: "Developer",
-        online: true,
-        date: "24/12/08",
+        no: "4",
+        nrp: "123456788",
+        nama: "John Michael",
+        pangkat: "Brada",
+        dikum: "S2",
+        dikpol: "Unknown",
+        fungsi: "Unknown",
+        diklat: "Unknown",
+        dikbangpes: "Unknown",
+        date: "23/04/18",
     },
     {
-        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-5.jpg",
-        name: "Richard Gran",
-        email: "richard@creative-tim.com",
-        job: "Manager",
-        org: "Executive",
-        online: false,
-        date: "04/10/21",
+        no: "5",
+        nrp: "123456788",
+        nama: "John Michael",
+        pangkat: "Brada",
+        dikum: "S2",
+        dikpol: "Unknown",
+        fungsi: "Unknown",
+        diklat: "Unknown",
+        dikbangpes: "Unknown",
+        date: "23/04/18",
     },
 ];
 
-export function MembersTable() {
+export default function TableDashboard() {
     return (
-        <Card className="h-full w-full">
+        <Card className="h-full w-full rounded-md">
             <CardHeader floated={false} shadow={false} className="rounded-none">
-                <div className="mb-8 flex items-center justify-between gap-8">
+                <div className="flex items-center justify-between">
                     <div>
-                        <Typography variant="h5" color="blue-gray">
-                            Members list
-                        </Typography>
-                        <Typography color="gray" className="mt-1 font-normal">
-                            See information about all members
-                        </Typography>
+                        <h1 className="text-xl font-bold text-black">
+                            Data Baru Ditambahkan
+                        </h1>
+                        <p color="gray" className="mt-1 font-normal">
+                            Informasi data yang baru saja ditambahkan
+                        </p>
                     </div>
-                    <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
-                        <Button variant="outlined" size="sm">
-                            view all
-                        </Button>
-                        <Button className="flex items-center gap-3" size="sm">
-                            <UserPlusIcon strokeWidth={2} className="h-4 w-4" />{" "}
-                            Add member
-                        </Button>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
-                    <Tabs value="all" className="w-full md:w-max">
-                        <TabsHeader>
-                            {TABS.map(({ label, value }) => (
-                                <Tab key={value} value={value}>
-                                    &nbsp;&nbsp;{label}&nbsp;&nbsp;
-                                </Tab>
-                            ))}
-                        </TabsHeader>
-                    </Tabs>
-                    <div className="w-full md:w-72">
-                        <Input
-                            label="Search"
-                            icon={<MagnifyingGlassIcon className="h-5 w-5" />}
-                        />
+                    <div className="flex shrink-0 gap-2 flex-row justify-end">
+                        <div className="w-full md:w-72">
+                            <Input
+                                label="Search"
+                                icon={
+                                    <MagnifyingGlassIcon className="h-5 w-5" />
+                                }
+                            />
+                        </div>
                     </div>
                 </div>
             </CardHeader>
-            <CardBody className="overflow-scroll px-0">
-                <table className="mt-4 w-full min-w-max table-auto text-left">
+            <CardBody className="overflow-x-scroll px-0">
+                <table className="w-full table-auto text-left">
                     <thead>
                         <tr>
-                            {TABLE_HEAD.map((head) => (
+                            {TABLE_HEAD.map((head, index) => (
                                 <th
                                     key={head}
-                                    className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
+                                    className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50"
                                 >
-                                    <Typography
-                                        variant="small"
-                                        color="blue-gray"
-                                        className="font-normal leading-none opacity-70"
-                                    >
-                                        {head}
-                                    </Typography>
+                                    <p className="flex items-center justify-between gap-2 font-normal leading-none text-sm text-black">
+                                        {head}{" "}
+                                        {index !== TABLE_HEAD.length - 1 && (
+                                            <ChevronUpDownIcon
+                                                strokeWidth={2}
+                                                className="h-4 w-4"
+                                            />
+                                        )}
+                                    </p>
                                 </th>
                             ))}
                         </tr>
@@ -146,7 +141,17 @@ export function MembersTable() {
                     <tbody>
                         {TABLE_ROWS.map(
                             (
-                                { img, name, email, job, org, online, date },
+                                {
+                                    no,
+                                    nrp,
+                                    nama,
+                                    pangkat,
+                                    dikum,
+                                    diklat,
+                                    dikpol,
+                                    dikbangpes,
+                                    fungsi,
+                                },
                                 index
                             ) => {
                                 const isLast = index === TABLE_ROWS.length - 1;
@@ -155,76 +160,63 @@ export function MembersTable() {
                                     : "p-4 border-b border-blue-gray-50";
 
                                 return (
-                                    <tr key={name}>
+                                    <tr key={nrp}>
                                         <td className={classes}>
                                             <div className="flex items-center gap-3">
-                                                <Avatar
-                                                    src={img}
-                                                    alt={name}
-                                                    size="sm"
-                                                />
                                                 <div className="flex flex-col">
-                                                    <Typography
-                                                        variant="small"
-                                                        color="blue-gray"
-                                                        className="font-normal"
-                                                    >
-                                                        {name}
-                                                    </Typography>
-                                                    <Typography
-                                                        variant="small"
-                                                        color="blue-gray"
-                                                        className="font-normal opacity-70"
-                                                    >
-                                                        {email}
-                                                    </Typography>
+                                                    <p className="font-normal text-sm text-black">
+                                                        {no}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td className={classes}>
+                                            <div className="flex items-center gap-3">
+                                                <div className="flex flex-col">
+                                                    <p className="font-normal text-sm text-black">
+                                                        {nrp}
+                                                    </p>
                                                 </div>
                                             </div>
                                         </td>
                                         <td className={classes}>
                                             <div className="flex flex-col">
-                                                <Typography
-                                                    variant="small"
-                                                    color="blue-gray"
-                                                    className="font-normal"
-                                                >
-                                                    {job}
-                                                </Typography>
-                                                <Typography
-                                                    variant="small"
-                                                    color="blue-gray"
-                                                    className="font-normal opacity-70"
-                                                >
-                                                    {org}
-                                                </Typography>
+                                                <p className="font-normal text-sm text-black">
+                                                    {nama}
+                                                </p>
                                             </div>
                                         </td>
                                         <td className={classes}>
-                                            <div className="w-max">
-                                                <Chip
-                                                    variant="ghost"
-                                                    size="sm"
-                                                    value={
-                                                        online
-                                                            ? "online"
-                                                            : "offline"
-                                                    }
-                                                    color={
-                                                        online
-                                                            ? "green"
-                                                            : "blue-gray"
-                                                    }
-                                                />
+                                            <div className="flex flex-col">
+                                                <p className="font-normal text-sm text-black">
+                                                    {pangkat}
+                                                </p>
                                             </div>
                                         </td>
                                         <td className={classes}>
-                                            <Typography
-                                                variant="small"
-                                                color="blue-gray"
-                                                className="font-normal"
-                                            >
-                                                {date}
-                                            </Typography>
+                                            <p className="font-normal text-sm text-black">
+                                                {dikum}
+                                            </p>
+                                        </td>
+                                        <td className={classes}>
+                                            <p className="font-normal text-sm text-black">
+                                                {dikpol}
+                                            </p>
+                                        </td>
+                                        <td className={classes}>
+                                            <p className="font-normal text-sm text-black">
+                                                {fungsi}
+                                            </p>
+                                        </td>
+                                        <td className={classes}>
+                                            <p className="font-normal text-sm text-black">
+                                                {diklat}
+                                            </p>
+                                        </td>
+                                        <td className={classes}>
+                                            <p className="font-normal text-sm text-black">
+                                                {dikbangpes}
+                                            </p>
                                         </td>
                                         <td className={classes}>
                                             <Tooltip content="Edit User">
