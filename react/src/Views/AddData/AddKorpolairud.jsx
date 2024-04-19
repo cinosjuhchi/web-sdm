212848f3f811fb591c82424a15f5b7d28e557985
Azepp <zidanathallahwinata@gmail.com>
2024-04-19 22:32:19 +0700

    form10%

--- react/src/Views/AddData/AddKorpolairud.jsx
+++ react/src/Views/AddData/AddKorpolairud.jsx
@@ -33,15 +33,15 @@ function AddKorpolairud() {
                             htmlFor="first-name"
                             className="block text-sm font-medium leading-6 text-gray-900"
                         >
-                            First name
+                            Nama
                         </label>
                         <div className="mt-2">
                             <input
                                 type="text"
-                                name="first-name"
-                                id="first-name"
+                                name="name"
+                                id="name"
                                 autoComplete="given-name"
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
                             />
                         </div>
                     </div>
@@ -51,20 +51,20 @@ function AddKorpolairud() {
                             htmlFor="last-name"
                             className="block text-sm font-medium leading-6 text-gray-900"
                         >
-                            Last name
+                            NRP
                         </label>
                         <div className="mt-2">
                             <input
                                 type="text"
-                                name="last-name"
-                                id="last-name"
+                                name="nrp"
+                                id="nrp"
                                 autoComplete="family-name"
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
                             />
                         </div>
                     </div>
 
-                    <div className="sm:col-span-4">
+                    <div className="sm:col-span-3">
                         <label
                             htmlFor="email"
                             className="block text-sm font-medium leading-6 text-gray-900"
--- react/src/Views/AddData/OptionAdd.jsx
+++ react/src/Views/AddData/OptionAdd.jsx
@@ -23,31 +23,36 @@ function OptionAdd() {
                     Tambahkan Data Baru
                 </h1>
             </div>
-            <div className="body flex justify-center items-center h-[calc(100vh-8rem)] gap-4">
-                <a href="/tambah-data-korpolairud">
-                    <div className="card flex flex-col justify-center items-center w-72 h-72 rounded-md bg-biru bg-opacity-50 hover:bg-biru duration-300 transition-all hover:scale-105">
-                        <img src={LogoKor} alt="" className="w-36" />
-                        <h1 className="mt-4 text-lg font-medium">
-                            Korpolairud
-                        </h1>
-                    </div>
-                </a>
-                <a href="/tambah-data-ditpolairud">
-                    <div className="card flex flex-col justify-center items-center w-72 h-72 rounded-md bg-biru bg-opacity-50 hover:bg-biru duration-300 transition-all hover:scale-105">
-                        <img src={LogoDit} alt="" className="w-40" />
-                        <h1 className="mt-4 text-lg font-medium">
-                            Ditpolairud
-                        </h1>
-                    </div>
-                </a>
-                <a href="/tambah-data-ditpoludara">
-                    <div className="card flex flex-col justify-center items-center w-72 h-72 rounded-md bg-biru bg-opacity-50 hover:bg-biru duration-300 transition-all hover:scale-105">
-                        <img src={LogoUdara} alt="" className="w-40" />
-                        <h1 className="mt-4 text-lg font-medium">
-                            Ditpoludara
-                        </h1>
-                    </div>
-                </a>
+            <div className="body flex flex-col justify-center items-center h-[calc(100vh-8rem)] gap-4">
+                <h1 className="text-center self-center font-medium text-lg mb-2">
+                    Pilih untuk melanjutkan
+                </h1>
+                <div className="flex gap-4">
+                    <a href="/tambah-data-korpolairud">
+                        <div className="card flex flex-col justify-center items-center w-72 h-72 rounded-md bg-biru bg-opacity-50 hover:bg-biru duration-300 transition-all hover:scale-105">
+                            <img src={LogoKor} alt="" className="w-36" />
+                            <h1 className="mt-4 text-lg font-medium">
+                                Korpolairud
+                            </h1>
+                        </div>
+                    </a>
+                    <a href="/tambah-data-ditpolairud">
+                        <div className="card flex flex-col justify-center items-center w-72 h-72 rounded-md bg-biru bg-opacity-50 hover:bg-biru duration-300 transition-all hover:scale-105">
+                            <img src={LogoDit} alt="" className="w-40" />
+                            <h1 className="mt-4 text-lg font-medium">
+                                Ditpolairud
+                            </h1>
+                        </div>
+                    </a>
+                    <a href="/tambah-data-ditpoludara">
+                        <div className="card flex flex-col justify-center items-center w-72 h-72 rounded-md bg-biru bg-opacity-50 hover:bg-biru duration-300 transition-all hover:scale-105">
+                            <img src={LogoUdara} alt="" className="w-40" />
+                            <h1 className="mt-4 text-lg font-medium">
+                                Ditpoludara
+                            </h1>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
     );
